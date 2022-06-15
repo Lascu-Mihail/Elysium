bd04f9986877670518f1d05ecf454f3dc55b0811
Mihai <80095435+Jansky23@users.noreply.github.com>
2022-06-15 09:43:07 +0300

    pom and start

---
+++ src/main/java/com/project/Elysium/ElysiumApplication.java
@@ -0,0 +1,13 @@
+package com.project.Elysium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElysiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElysiumApplication.class, args);
+	}
+
+}
